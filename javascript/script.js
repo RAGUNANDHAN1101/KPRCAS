4b5ca7c6a6e4c69f48225a73ed3893ff1d7c5a74
Ragups <raguragu87266@gmail.com>
2025-06-14 15:54:47 +0530

    commit message

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+function sum(a,b) {
+    console.log(a+b)
+}
+sum(10,20)
+
+var sum = ()=>{
+    var a = 10;
+    var b = 20;
+    console.log(a+b)
+}
+sum();
+let arr =[10,20,30,40];
+let arr2 =[50,60,70];
+console.log(arr);
+console.log(arr2);
